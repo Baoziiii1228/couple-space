aa34d7c2fe2b91d2a311b38f8f1924c3867a38d1
Baoziiii1228 <244356222+Baoziiii1228@users.noreply.github.com>
2026-02-08 04:45:41 -0500

    feat: 使用 Manus 内置邮件服务替换 Resend

--- server/_core/email.ts
+++ server/_core/email.ts
@@ -1,6 +1,6 @@
-import { sendVerificationCodeViaResend } from "./resend-email";
+import { sendVerificationCodeViaManus } from "./manus-email";
 
 export async function sendVerificationCode(email: string, code: string): Promise<boolean> {
-  // 使用 Resend 邮件服务发送验证码
-  return sendVerificationCodeViaResend(email, code);
+  // 使用 Manus 内置邮件服务发送验证码
+  return sendVerificationCodeViaManus(email, code);
 }
